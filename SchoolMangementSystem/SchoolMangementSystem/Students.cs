5d8975039d068db9b2324a9e63e293db28624ab2
Cezmi <cezmi.aktepe04@gmail.com>
2021-04-17 02:13:19 -0400

    Students class has a new field

--- SchoolMangementSystem/SchoolMangementSystem/Students.cs
+++ SchoolMangementSystem/SchoolMangementSystem/Students.cs
@@ -10,11 +10,13 @@ namespace SchoolMangementSystem
     {
         private int rollNumber;
         private DateTime regDate;
+        private double englishMark;
 
-        public Students(string fname, string lname, int rollNumber, DateTime regDate) : base(fname, lname)
+        public Students(string fname, string lname, int rollNumber, DateTime regDate, double englishMark) : base(fname, lname)
         {
             this.rollNumber = rollNumber;
             this.regDate = regDate;
+            this.englishMark = englishMark;
         }
     }
 }

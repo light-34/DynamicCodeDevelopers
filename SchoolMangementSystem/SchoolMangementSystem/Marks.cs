efa6d0d2f78c53f43d30d8d7afa902a156a16d5a
Cezmi <cezmi.aktepe04@gmail.com>
2021-04-29 05:45:43 -0400

    Cezmi added Hashtable to the App

--- SchoolMangementSystem/SchoolMangementSystem/Marks.cs
+++ SchoolMangementSystem/SchoolMangementSystem/Marks.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace SchoolMangementSystem
         private double midtermTwo;
         private double midtermThree;
         private double final;
+        private Hashtable avHash = new Hashtable();
+        
 
         public Marks () { }
 
@@ -26,27 +29,66 @@ namespace SchoolMangementSystem
         //This method is designed to get data from user
         public void getMarks ()
         {
-            Console.WriteLine("                 Average Calculation System");
-            Console.WriteLine("**********************************************************\n");
-            Console.WriteLine("Please enter Student's Name");
-            string name = Console.ReadLine();
-            Console.WriteLine("\nPlease enter Student's Last Name");
-            string lname = Console.ReadLine();
-            Console.WriteLine("\nPlease enter Student's Roll Number");
-            int roll = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nEnter first midterm mark");
-            double midOne = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\nEnter second midterm mark");
-            double midTwo = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\nEnter third midterm mark");
-            double midThree = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("\nEnter final exam mark");
-            double final = Convert.ToDouble(Console.ReadLine());
+            string inChoice = "y";
+            do {
+                Console.WriteLine("                 Average Calculation System");
+                Console.WriteLine("**********************************************************\n");
+                Console.WriteLine("Please enter Student's Name");
+                string name = Console.ReadLine();
+                Console.WriteLine("\nPlease enter Student's Last Name");
+                string lname = Console.ReadLine();
+                Console.WriteLine("\nPlease enter Student's Roll Number");
+                int roll = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nEnter first midterm mark");
+                double midOne = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("\nEnter second midterm mark");
+                double midTwo = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("\nEnter third midterm mark");
+                double midThree = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("\nEnter final exam mark");
+                double final = Convert.ToDouble(Console.ReadLine());
 
-            Students student = new Students(name, lname, roll);
-            double average = semesterAverageMark(new Marks(midOne, midTwo, midThree, final));
-            printMarks(student, average);
-           
+                Students student = new Students(name, lname, roll);
+                double average = semesterAverageMark(new Marks(midOne, midTwo, midThree, final));
+                printMarks(student, average);
+
+                Console.WriteLine("Do you want to save this information? (Y/N)");
+                string choice = Console.ReadLine();
+
+                if (String.Equals(choice, "y", StringComparison.OrdinalIgnoreCase))
+                {
+                    avHash.Add(student.getRollNumber(), average);
+                    Console.WriteLine("Data Saved\n");
+                    Console.WriteLine("**********************************************************\n");
+                }
+                else
+                {
+                    Console.WriteLine("Data Deleted\n");
+                    Console.WriteLine("**********************************************************\n");
+                }
+
+                Console.WriteLine("Do you want to continue to calculate? (Y/N)");
+                inChoice = Console.ReadLine();
+
+            } while (String.Equals(inChoice, "y", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine("Do you want to display all average marks? (Y/N)");
+            string avMarkChoice = Console.ReadLine();
+            if (String.Equals(avMarkChoice, "y", StringComparison.OrdinalIgnoreCase))
+            {
+                foreach (DictionaryEntry item in avHash)
+                {
+                    Console.WriteLine($"Roll No : {item.Key} - Average : {item.Value}");
+                }
+                Console.WriteLine("\n**********************************************************\n");
+            }
+            else
+            {
+                Console.WriteLine("Module Terminated\n");
+                Console.WriteLine("**********************************************************\n");
+            }
+
+
         }
 
         //This method is designed to get a Marks type parameter
@@ -66,9 +108,9 @@ namespace SchoolMangementSystem
         //a double parameter to display data
         public void printMarks(Students student, double average)
         {
-            Console.WriteLine("*************************************************************************\n");
+            Console.WriteLine("**********************************************************\n");
             Console.WriteLine($"Student Avarage Mark Info \nFull Name : {student.getLName()}, {student.getFName()} \nRoll Number : {student.getRollNumber()} \nAverage Mark : {average}\n");
-            Console.WriteLine("*************************************************************************\n");
+            Console.WriteLine("**********************************************************\n");
         }
     }
 }
